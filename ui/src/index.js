14f5868925fd2347219f2520b3a7346e716f13ac
Mike Wooldridge <mike@hellion.org>
2023-03-24 17:33:51 -0700

    WTF?

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Results from './Results';
-import Search from './Search';
-import MyGrid from './MyGrid';
-import MyListView from './MyListView';
 import MyMap from './MyMap';
-import { ListView } from "@progress/kendo-react-listview";
 import { MLProvider } from './ML';
-import searchResults from './searchResults.json';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-
-import { Pager } from "@progress/kendo-react-data-tools";
-
-import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
@@ -25,26 +13,8 @@ root.render(
       host="localhost"
       port="4000"
     >
-      <div style={{padding: '10px'}}>
-        <Tabs
-          defaultActiveKey="mapView"
-          id="fill-tab-example"
-          className="mb-3"
-          fill
-        >
-          <Tab eventKey="listView" title="ListView">
-            <Search label="Search!"/>
-            {/* <Results/>  */}
-            <MyListView 
-              data={searchResults.results}
-            />
-          </Tab>
-          <Tab eventKey="mapView" title="Map">
-            <div>
-              <MyMap />
-            </div>
-          </Tab>
-        </Tabs>
+      <div>
+        <MyMap />
       </div>
     </MLProvider>
     <style>
