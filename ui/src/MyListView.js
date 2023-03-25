802187f19d0344d63e5d33eb5b08ac0aaec29e31
Mike Wooldridge <mike@hellion.org>
2023-03-24 18:04:02 -0700

    WTF5

--- ui/src/MyListView.js
+++ ui/src/MyListView.js
@@ -1,13 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { ListView } from "@progress/kendo-react-listview";
-import { Pager } from "@progress/kendo-react-data-tools";
-import { Avatar } from "@progress/kendo-react-layout";
-import { DateTime } from "luxon";
-import MLContext from './ML';
 import '@progress/kendo-theme-bootstrap/dist/all.css';
 
 const MyListView = () => {
-
   return (
     <div>
     </div>
